efd3bde2723ee2d2197613d6ef9d8b79418d7f78
Agnes Kiss <agnes@snowplowanalytics.com>
2026-02-13 10:04:05 +0000

    Update package yml with autofix

--- models/snowplow_identities_id_mapping_scd.sql
+++ models/snowplow_identities_id_mapping_scd.sql
@@ -5,27 +5,24 @@ and you may not use this file except in compliance with the Snowplow Personal an
 You may obtain a copy of the Snowplow Personal and Academic License Version 1.0 at https://docs.snowplow.io/personal-and-academic-license-1.0/
 #}
 
-{{
-  config(
-    materialized='incremental',
-    on_schema_change='append_new_columns',
-    unique_key='id_change_key',
-    upsert_date_key='effective_at',
-    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')),
-    partition_by = snowplow_utils.get_value_by_target_type(bigquery_val = {
+{{ config(
+    materialized="incremental", 
+    on_schema_change="append_new_columns", 
+    unique_key="id_change_key", 
+    sql_header=snowplow_utils.set_query_tag(var('snowplow__query_tag', 'snowplow_dbt')), 
+    partition_by=snowplow_utils.get_value_by_target_type(bigquery_val = {
       "field": "effective_at",
       "data_type": "timestamp",
       "granularity": "day"
-    }, databricks_val='effective_at_date'),
-    cluster_by=snowplow_identities.get_cluster_by_values('id_mapping_scd'),
-    tags=["derived"],
+    }, databricks_val='effective_at_date'), 
+    cluster_by=snowplow_identities.get_cluster_by_values('id_mapping_scd'), 
+    tags=["derived"], 
     tblproperties={
       'delta.autoOptimize.optimizeWrite' : 'true',
       'delta.autoOptimize.autoCompact' : 'true'
-    },
-    snowplow_optimize=true
-  )
-}}
+    }, 
+    meta={'upsert_date_key': 'effective_at', 'snowplow_optimize': true}
+) }}
 
 WITH ids_affected_this_run AS (
     SELECT DISTINCT snowplow_id 
